aaf0261095a99d0d779c9c612d24ea7d48d14918
tradm <starsona@gmx.com>
2023-12-10 07:03:24 +0100

    first commit
    set up development

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="text-black">
+      Hello world
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#004777",
+        secondary: "#0A122A"
+      }
+    },
+  },
+  plugins: [],
+}
