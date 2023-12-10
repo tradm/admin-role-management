36544f79d076ac20a8fb2682468f5cc4c5423799
tradm <starsona@gmx.com>
2023-12-10 07:15:31 +0100

    feat: react router
    implement react router for app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import User from "./User"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <User/>
+  },
+  {
+    path: "login",
+    element: <Login/>,
+  },
+]);
+
+
 function App() {
 
   return (
     <div className="text-black">
-      Hello world
+      <RouterProvider router={router} />
     </div>
   )
 }
